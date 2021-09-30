feb3c9fdb6fe2b55ab10418c74461ded4942c964
spoodr <70950932+IBgreat1@users.noreply.github.com>
2021-09-30 14:36:28 +0530

    Create Dash.lua

---
+++ Examples/Dash.lua
@@ -0,0 +1,25 @@
+local Input = require(game.ReplicatedStorage.Input)
+local Return = Input.Sequence.new(.5,Enum.KeyCode.W,Enum.KeyCode.W)
+local Character
+
+--This is a basic script to teleport 10 studs ahead upon double tapping W to showcase how the module works.
+
+local function Setup(char)
+    if not char:FindFirstChild('HumanoidRootPart') then
+        char:WaitForChild('HumanoidRootPart')
+    end
+    Character = char
+end
+game.Players.LocalPlayer.CharacterAdded:Connect(Setup)
+
+Return.Pressed:Connect(function(inp,gp,index)
+    if gp then
+        Return:Cancel()
+    elseif index == 2 and Character then
+        Character.HumanoidRootPart.CFrame *= CFrame.new(0,0,-10)
+    end
+end)
+
+Return.End:Connect(function()
+    Return:Toggle(false,2)
+end)
