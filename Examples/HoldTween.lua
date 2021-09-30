38a5026e6a94a9d10469c6dffaa199e791897cfd
spoodr <70950932+IBgreat1@users.noreply.github.com>
2021-09-30 15:22:21 +0530

    Create HoldTween.lua

---
+++ Examples/HoldTween.lua
@@ -0,0 +1,37 @@
+local TWS = game:GetService('TweenService')
+local Input = require(game.ReplicatedStorage.Input)
+local Hold = Input.Hold.new(Enum.UserInputType.MouseButton1)
+local Up 
+local Part = Instance.new('Part')
+Part.Anchored = true
+Part.Parent = workspace
+
+
+Hold.Start:Connect(function(gp)
+	if gp then return end
+	print('Start')
+	Up = TWS:Create(Part,TweenInfo.new(3),{Size = Vector3.new(4,10,2),CFrame = Part.CFrame * CFrame.new(0,4.5,0)})
+	Up:Play()
+	Up.Completed:Connect(function(Base)
+		if Base == Enum.PlaybackState.Completed then
+			print('Complete')
+			Hold:Destroy()
+			Up:Destroy()
+		end
+	end)
+end)
+
+Hold.Hold:Connect(function(Input,gp)
+	if gp then
+		Hold:Cancel(Input)
+	end
+end)
+
+Hold.End:Connect(function()
+	print('End')
+	Up:Cancel()
+	Part.Size = Vector3.new(4,1,2)
+	Part.Position = Vector3.new()
+	Up:Destroy()
+end)
+
