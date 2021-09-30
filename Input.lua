65d554a9e4ef299ccd7d827ef65695bc38798adf
spoodr <70950932+IBgreat1@users.noreply.github.com>
2021-09-30 14:27:23 +0530

    Create Input.lua

---
+++ Input.lua
@@ -0,0 +1,24 @@
+local TypeDefinitions = require(script:WaitForChild("TypeDefinitions"));
+type InputType = TypeDefinitions.Input;
+
+local Input : InputType = {}
+local Ignore = {'Settings','Utils', 'TypeDefinitions'}
+
+local UIS = game:GetService('UserInputService')
+
+for _ , v in ipairs(script:GetChildren()) do
+	if v:IsA('ModuleScript') and not table.find(Ignore,v.Name) then
+		Input[v.Name] = require(v)
+	end
+end
+
+UIS.InputBegan:Connect(function(...)
+	Input.Sequence.Hook(...)
+	Input.Hold.HookStart(...)
+end)
+
+UIS.InputEnded:Connect(function(...)
+	Input.Hold.HookEnd(...)
+end)
+
+return Input
